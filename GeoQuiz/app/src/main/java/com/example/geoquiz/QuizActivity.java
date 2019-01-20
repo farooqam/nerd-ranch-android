3471bead031c46bebbdc06a26e5646467436ed30
Farooq Mahmud <farooq.mahmud@marel.com>
2019-01-20 12:46:42 -0800

    Initial commit.

---
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -0,0 +1,13 @@
+package com.example.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_main);
+    }
+}
