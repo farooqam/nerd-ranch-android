dd2cdc62b5c344cb74e21778b888dbdd4846105d
Farooq Mahmud <farooq.mahmud@marel.com>
2019-01-21 07:28:39 -0800

    Remove unused imports.

--- GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -1,10 +1,9 @@
 package com.example.geoquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
--- GeoQuiz/app/src/main/java/com/example/geoquiz/Toaster.java
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/Toaster.java
@@ -1,7 +1,5 @@
 package com.example.geoquiz;
 
-import android.widget.Toast;
-
 public interface Toaster {
     void showToast(int message);
 }
